d1681afc3b8118f8f3b5527e16fd2752fe6a56ed
DonghaoFeng <380378504@qq.com>
2020-05-20 11:54:20 +0800

    updated

--- app.read/cpp/ReadTableJNI.cpp
+++ app.read/cpp/ReadTableJNI.cpp
@@ -2,60 +2,71 @@
 #include <string.h>
 #include <stdio.h>
 #include <ReadTableJNI.h>
+#include <iconv.h>
 #include "oodb_cpp_apiop.h"
 using namespace oodb;
 
-int switch_type(int type, char *value) {
+char* charset_convert(const char* from_charset,const char *to_charset,char *in_buf);
+
+void switch_type(int type, char *value,string& str) {
+	char buff[128] = {0};
 	switch (type) {
 	case OODB_CHAR:
-		printf("%s|", value);
+		sprintf(buff,"%s|", value);
 		break;
 	case OODB_TIME: {
 		//printf("%d: %s\t",i,ctime((time_t*)value));
 		char *timeBuf = ctime((time_t*) value);
 		timeBuf[24] = '\0';
-		printf("%s|", timeBuf);
+		sprintf(buff,"%s|", timeBuf);
 		break;
 	}
 	case OODB_FLOAT:
-		printf("%f|",  *(float*) value);
+		sprintf(buff,"%f|",  *(float*) value);
 		break;
 	case OODB_DOUBLE:
-		printf("%lf|", *(double*) value);
+		sprintf(buff,"%lf|", *(double*) value);
 		break;
 	case OODB_INT:
-		printf("%d|",  *(int*) value);
+		sprintf(buff,"%d|",  *(int*) value);
 		break;
 	case OODB_SMALLINT:
-		printf("%d|", *(smallint*) value);
+		sprintf(buff,"%d|", *(smallint*) value);
 		break;
 	case OODB_TINYINT:
-		printf("%d|", *(tinyint*) value);
+		sprintf(buff,"%d|", *(tinyint*) value);
 		break;
 	case OODB_BIT:
-		printf("%d|",*(bit*) value);
+		sprintf(buff,"%d|",*(bit*) value);
 		break;
 	case OODB_MASK:
-		printf("%d|",  *(mask*) value);
+		sprintf(buff,"%d|",  *(mask*) value);
 		break;
 	case OODB_LONG:
-		printf("%d|", *(longint*) value);
-		break;
+		sprintf(buff,"%d|", *(longint*) value);
 		//case OODB_UINT		:
 		//printf("%d: %d\t",i,*(unsigned int*)value);
 		//break;
 	case OODB_PAIR_LONG:
 	case OODB_MEASID: {
 		PAIR_LONG *pair_long = (PAIR_LONG*) value;
-		printf("%ld,%ld|",pair_long->first_long,
+		sprintf(buff,"%ld,%ld|",pair_long->first_long,
 				pair_long->second_long);
 		break;
 	}
 	default:
 		printf("unknow ");
-		break;
 
 	}
+	switch (type) {
+	case OODB_CHAR:
+		str.append(charset_convert("gb2312","utf-8", buff));
+		break;
+	default:
+		str.append(buff);
+	}
+
+//	printf(str.c_str());
 }
 
 JNIEXPORT jstring JNICALL
@@ -68,43 +79,79 @@ JNIEXPORT jstring JNICALL
 	char* tb = (char*)env->GetStringUTFChars(tb_name,0);
 	int ret = oodbapi.OpenTb(sys, bob, db, tb);
 	if (ret < 0) {
-		printf("OpenTb() 获取失败!\n");
+		printf("OpenTb() 失败!\n");
 	}else{
-		printf("OpenTb() 获取成功!\n");
+		printf("OpenTb() 成功!\n");
 	}
 
 	CResult read_result;
-	/*……………*/
+	/*锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷*/
 	ret = oodbapi.ReadTb(read_result, 1);
 	if (ret < 0) {
-		printf("ReadTb() 获取失败!\n");
+		printf("ReadTb() 失败!\n");
 	}else{
-		printf("ReadTb() 获取成功!\n");
+		printf("ReadTb() 成功!\n");
 	}
-	printf((char *)(read_result.buffer));
-	int rec_num = read_result.rec_num;//表记录数
-	int rec_len = read_result.rec_len;//记录长度
+//	printf((char *)(read_result.buffer));
+	int rec_num = read_result.rec_num;
+	int rec_len = read_result.rec_len;
 	vector < ATTRDESC > attr_desc;
 	ret = oodbapi.GetAttrDesc(attr_desc);
-	int attr_num = attr_desc.size();//属性个数
+	int attr_num = attr_desc.size();
 	char* attr_value =(char*) read_result.buffer;
 	char* attr_values = attr_value;
+	string str;
 	for(int i =0; i< rec_num;i++)
 	{
-		printf("记录%05d:",i+1);
-		//cout<<"第"<<i+1<<"条记录:";
 		for(int k = 0; k<attr_num; k++)
 		{
 			attr_values= attr_value + attr_desc[k].offset;
-			switch_type(attr_desc[k].type,attr_values);
+			switch_type(attr_desc[k].type,attr_values,str);
+
 		}
 		attr_value+= rec_len;
-		cout<<endl<<endl;
+		str.append("\n");
+//		cout<<endl<<endl;
 	}
-	char buff[128] = {0};
-	float a = 0.1;
-	sprintf(buff, "%f", a);
-	return (jstring)(env)->NewStringUTF(buff);
+
+	return (jstring)(env)->NewStringUTF(str.c_str());
 }
 
+char* charset_convert(const char* from_charset,const char *to_charset,char *in_buf)
+{
+
+	size_t in_left = strlen(in_buf);
+	size_t out_left = in_left*4;
+	char *out_buf = (char *)malloc(sizeof(char)*out_left);
+	size_t sRet = -1;
+	char *pIn = in_buf;
+	char *pOut = out_buf;
+	size_t outLen = out_left;
+	iconv_t icd = (iconv_t)-1;
+
+	if ((NULL == from_charset) || (NULL == to_charset))
+	{
+		return "-1";
+	}
+
+	icd = iconv_open( to_charset,from_charset);
+	if (((iconv_t)-1)  == icd)
+	{
+		return "-2";
+	}
+//	printf("inLen=%d,outLen=%d\n",in_left,out_left);
+
+	sRet = iconv(icd,&pIn,&in_left,&pOut,&out_left);
+	if (((size_t)-1) == sRet)
+	{
+		printf("errno=%d\n",errno);
+		return "-3";
+	}
+
+//	printf("in_left=%d,out_left=%d\n",in_left,out_left);
+	out_buf[outLen-out_left] = 0;
+	iconv_close(icd);
+	return out_buf;
+
+}
 
--- app.read/src/testJNI.java
+++ app.read/src/testJNI.java
@@ -1,8 +1,8 @@
-
-
 public class testJNI {
 
 	public static void main(String[] args) {
-		System.out.println(ReadTableJNI.readTable("realtime", "scada", "psdb", "substation"));
+		System.out.println(System.currentTimeMillis());
+		ReadTableJNI.readTable("realtime", "scada", "psdb", "substation");
+		System.out.println(System.currentTimeMillis());
 	}
 }
