138323bfecf9cb670b75d5662b1b8e3a21d186c2
DonghaoFeng <380378504@qq.com>
2020-06-10 14:20:31 +0800

    performance only

--- app.read/src/app/read/bean/Wams_his_dataRecord.java
+++ app.read/src/app/read/bean/Wams_his_dataRecord.java
@@ -1,137 +1,120 @@
 package app.read.bean;
 
-import java.util.ArrayList;
-
 public class Wams_his_dataRecord extends BaseJSONBean {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 
 	public Wams_his_dataRecord() {
 		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	
-	public Wams_his_dataRecord(ArrayList<PointRecord> pointList) {
-		super();
-		this.pointList = pointList;
 	}
 
-
-	public Wams_his_dataRecord(String dataStr) {
-		String[] attributeStr = dataStr.split("\\|");
-//		for (int i = 0; i < attributeStr.length; i++) {
-//			System.out.print(attributeStr[i]+",");
-//		}
-		this.record_id = Integer.valueOf(attributeStr[0]);
-		this.psid = Long.valueOf(attributeStr[1].split(",")[0] + attributeStr[1].split(",")[1]);
-		for (int i = 0; i < 50; i++) {
-			this.pointList.add(new PointRecord(Double.valueOf(attributeStr[2 * (i + 1)]),
-					Integer.valueOf(attributeStr[2 * (i + 1) + 1])));
-		}
-	}
 	public int record_id;
-	private long psid;
-	private ArrayList<PointRecord> pointList  = new ArrayList<PointRecord>();
-	private int status1;
-	private int status2;
-	private int status3;
-	private int status4;
-	private int status5;
-	private int status6;
-	private int status7;
-	private int status8;
-	private int status9;
-	private int status10;
-	private int status11;
-	private int status12;
-	private int status13;
-	private int status14;
-	private int status15;
-	private int status16;
-	private int status17;
-	private int status18;
-	private int status19;
-	private int status20;
-	private int status21;
-	private int status22;
-	private int status23;
-	private int status24;
-	private int status25;
-	private int status26;
-	private int status27;
-	private int status28;
-	private int status29;
-	private int status30;
-	private int status31;
-	private int status32;
-	private int status33;
-	private int status34;
-	private int status35;
-	private int status36;
-	private int status37;
-	private int status38;
-	private int status39;
-	private int status40;
-	private int status41;
-	private int status42;
-	private int status43;
-	private int status44;
-	private int status45;
-	private int status46;
-	private int status47;
-	private int status48;
-	private int status49;
-	private int status50;
+	public long psid;
+	public int status1;
+	public int status2;
+	public int status3;
+	public int status4;
+	public int status5;
+	public int status6;
+	public int status7;
+	public int status8;
+	public int status9;
+	public int status10;
+	public int status11;
+	public int status12;
+	public int status13;
+	public int status14;
+	public int status15;
+	public int status16;
+	public int status17;
+	public int status18;
+	public int status19;
+	public int status20;
+	public int status21;
+	public int status22;
+	public int status23;
+	public int status24;
+	public int status25;
+	public int status26;
+	public int status27;
+	public int status28;
+	public int status29;
+	public int status30;
+	public int status31;
+	public int status32;
+	public int status33;
+	public int status34;
+	public int status35;
+	public int status36;
+	public int status37;
+	public int status38;
+	public int status39;
+	public int status40;
+	public int status41;
+	public int status42;
+	public int status43;
+	public int status44;
+	public int status45;
+	public int status46;
+	public int status47;
+	public int status48;
+	public int status49;
+	public int status50;
 	
-	private float value1;
-	private float value2;
-	private float value3;
-	private float value4;
-	private float value5;
-	private float value6;
-	private float value7;
-	private float value8;
-	private float value9;
-	private float value10;
-	private float value11;
-	private float value12;
-	private float value13;
-	private float value14;
-	private float value15;
-	private float value16;
-	private float value17;
-	private float value18;
-	private float value19;
-	private float value20;
-	private float value21;
-	private float value22;
-	private float value23;
-	private float value24;
-	private float value25;
-	private float value26;
-	private float value27;
-	private float value28;
-	private float value29;
-	private float value30;
-	private float value31;
-	private float value32;
-	private float value33;
-	private float value34;
-	private float value35;
-	private float value36;
-	private float value37;
-	private float value38;
-	private float value39;
-	private float value40;
-	private float value41;
-	private float value42;
-	private float value43;
-	private float value44;
-	private float value45;
-	private float value46;
-	private float value47;
-	private float value48;
-	private float value49;
-	private float value50;
+	public float value1;
+	public float value2;
+	public float value3;
+	public float value4;
+	public float value5;
+	public float value6;
+	public float value7;
+	public float value8;
+	public float value9;
+	public float value10;
+	public float value11;
+	public float value12;
+	public float value13;
+	public float value14;
+	public float value15;
+	public float value16;
+	public float value17;
+	public float value18;
+	public float value19;
+	public float value20;
+	public float value21;
+	public float value22;
+	public float value23;
+	public float value24;
+	public float value25;
+	public float value26;
+	public float value27;
+	public float value28;
+	public float value29;
+	public float value30;
+	public float value31;
+	public float value32;
+	public float value33;
+	public float value34;
+	public float value35;
+	public float value36;
+	public float value37;
+	public float value38;
+	public float value39;
+	public float value40;
+	public float value41;
+	public float value42;
+	public float value43;
+	public float value44;
+	public float value45;
+	public float value46;
+	public float value47;
+	public float value48;
+	public float value49;
+	public float value50;
 	
 	
 	public long getRecord_id() {
@@ -146,25 +129,7 @@ public class Wams_his_dataRecord extends BaseJSONBean {
 	public void setPsid(long psid) {
 		this.psid = psid;
 	}
-	public ArrayList<PointRecord> getPointList() {
-		return pointList;
-	}
-	public void setPointList(ArrayList<PointRecord> pointList) {
-		this.pointList = pointList;
-	}
 	
-	public void ini() {
-		if (this.pointList.size() == 0) {
-			for (int i = 1; i <= 50; i++) {
-				try {
-					this.pointList.add(new PointRecord(this.getClass().getDeclaredField("value" + i).getDouble(this),
-							this.getClass().getDeclaredField("status" + i).getInt(this)));
-				} catch (NoSuchFieldException | SecurityException | IllegalArgumentException
-						| IllegalAccessException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+	
+	
 }
--- app.read/src/app/read/collection/DataHelper.java
+++ app.read/src/app/read/collection/DataHelper.java
@@ -2,25 +2,31 @@ package app.read.collection;
 
 import java.util.List;
 
+import app.read.bean.Wams_his_dataRecord;
+
 public class DataHelper {
-	
+
 	private FixSizeLinkedList<WAMSData> list;
 
 	public DataHelper(FixSizeLinkedList<WAMSData> list) {
 		super();
 		this.list = list;
 	}
-	
+
 	public double[] getData(int duration) {
 		List<WAMSData> subList = this.list.subList(this.list.size() - duration, this.list.size());
 		double[] yOrigin = new double[duration * 50];
-		list.parallelStream().forEach(data -> data.getRecord().ini());
 		for (int i = 0; i < duration; i++) {
 			for (int j = 0; j < 50; j++) {
-				yOrigin [i*50+j]=subList.get(i).getRecord().getPointList().get(j).getValue();
+				Wams_his_dataRecord record = subList.get(i).getRecord();
+				try {
+					yOrigin[i * 50 + j] = record.getClass().getDeclaredField("value" + (j + 1)).getDouble(record);
+				} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException
+						| SecurityException e) {
+					e.printStackTrace();
+				}
 			}
 		}
-		
 		return yOrigin;
 	}
 
